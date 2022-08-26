6b984c96bec31553ce19ee650b0f6c8f7677495b
Keerthigavijayakrishnan <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2022-08-26 22:39:05 +0530

    Update FindSecondLargest.java
    Week3.Day2- FindSecondLargest

--- week3/day1/Assignments/FindSecondLargest.java
+++ week3/day1/Assignments/FindSecondLargest.java
@@ -11,9 +11,32 @@ public class FindSecondLargest {
 		/*
 		 * Pseudo Code: 
 		 * a) Create a empty Set Using TreeSet
+		 Set <Integer> set = new TreeSet<Integer>();
+		 
 		 * b) Declare for loop iterator from 0 to data.length and add into Set 
+		  int length = set.size();
+		 int length1 = data.length;
+		
+		for(int i=0; i<=length1; i++) {
+			
+		System.out.println(i);
+	}
+		
+		for (int eachNumber: data) {
+			System.out.println(set.add(eachNumber));
+			 
+		 }
+		 
 		 * c) converted Set into List
+		 List<Integer> list = new ArrayList<Integer>(set);
+		 
 		 * d) Print the second last element from List
+		  Collections.sort(list);
+	         System.out.println(list);
+	          int secondlargestNumber = list.get(list.size()-2);
+	 
+		 System.out.println("SecondLargestNumber: "+secondlargestNumber);
+		 
 		 * 
 		 * 
 		 */
